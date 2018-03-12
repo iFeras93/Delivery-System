ad767b0f936312a2e09740613858cfee79975e34
iFeras93 <ferasmo7mad@gmail.com>
2018-03-12 11:45:50 +0200

    First commit

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // $this->call(UsersTableSeeder::class);
+        DB::table('users')->insert([
+            'name' => 'admin system',
+            'email' => 'admin@admin.com',
+            'password' => bcrypt(123456), // secret
+            'type' => 'admin',
+            'remember_token' => str_random(10),
+        ]);
+
+    }
+}
